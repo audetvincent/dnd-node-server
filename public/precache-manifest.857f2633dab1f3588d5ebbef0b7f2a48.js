4df72568aa4d4b64b88e932222e94719e578afde
Vincent Audet <vincent.audet@ubisoft.com>
2019-04-25 15:24:32 -0400

    Initial commit

---
+++ public/precache-manifest.857f2633dab1f3588d5ebbef0b7f2a48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "95fef60db00fbb1dec63",
+    "url": "/js/chunk-vendors.47350325.js"
+  },
+  {
+    "revision": "a91067cec03676ca0f55",
+    "url": "/js/app.d994b7c4.js"
+  },
+  {
+    "revision": "e3265da0b912786c641c",
+    "url": "/js/about.69260e02.js"
+  },
+  {
+    "revision": "9b2c5c6772860ab0c1ed2ade8f930004",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a91067cec03676ca0f55",
+    "url": "/css/app.afdbb8f3.css"
+  }
+];
